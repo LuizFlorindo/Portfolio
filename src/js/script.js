c60c8d6f75e9916f8d42f543efe38bb22c5bb396
LuizFlorindo <luizflorindo123@gmail.com>
2023-08-18 12:48:09 -0300

    Version 1.0 of my portfolio

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+// ALTERAR ID DA URL
+let URL_ANTIGA = window.location.href;
+let URL_NOVA = URL_ANTIGA.split("/")
+URL_NOVA.pop();
+URL_NOVA.push("#home")
+URL_NOVA = URL_NOVA.join("/");
+
+window.location.replace(URL_NOVA);
+
+
+
+// NAVBAR
+function open_or_close_navbar(){
+    document.getElementById("menu-navbar").classList.toggle("active-navbar");
+}
+
+
+
+
+
+// COPIAR EMAIL
+var content_copy;
+var modal = document.getElementById("modal");
+
+function copy_text(text){
+    content_copy = document.createElement("input");
+    content_copy.type = "text";
+
+    if(text == 'email'){
+        content_copy.value = "devluizsilvaofc@gmail.com"
+    }else if(text == 'num'){
+        content_copy.value = "+55 85 99236-1807";
+    }
+
+    if(navigator.clipboard.writeText(content_copy.value)){
+        modal.style.display = "flex";
+        setTimeout(()=>{
+            if(modal.style.display == "flex"){
+                modal.style.display = "none";
+            }
+        }, 1500);
+    }
+}
